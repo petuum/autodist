2b850dad6de2dd0677b52b0a0e35f3103fff3efe
Peng Wu <pengwu100@gmail.com>
2019-07-22 15:09:49 +0000

    Refine ArionDeviceSpec name string

---
+++ tests/test_device_spec.py
@@ -0,0 +1,13 @@
+from autodist.resource_spec import DeviceSpec, DeviceType
+
+
+def test_device_spec_string():
+    d1 = DeviceSpec(host_address='0.0.0.0')
+    s1 = d1.name_string()
+    assert s1 == '0.0.0.0:CPU:0'
+    assert DeviceSpec.from_string(s1).name_string() == s1
+
+    d2 = DeviceSpec(host_address='localhost', device_type=DeviceType.GPU, device_index=2)
+    s2 = d2.name_string()
+    assert s2 == 'localhost:GPU:2'
+    assert DeviceSpec.from_string(s2).name_string() == s2
