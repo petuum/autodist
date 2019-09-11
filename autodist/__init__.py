cb30bbab62bfcbec64248a5bd532335518f1a714
Peng Wu <pengwu100@gmail.com>
2019-09-11 21:42:55 +0000

    [SYM-8809] Experimental Patch

--- autodist/__init__.py
+++ autodist/__init__.py
@@ -1,1 +1,7 @@
+import os
+
+from .kernel.experimental.patch import PatchTensorFlow
 from .autodist import AutoDist
+
+if os.environ.get('AUTODIST_PATCH_TF', '') == '1':
+    PatchTensorFlow.patch_var_reading()
