3b9c17ce43b936a7f14a3f824cd2b960c75e1b4f
Hao Zhang <hao@cs.cmu.edu>
2019-10-17 18:07:43 +0000

    [SYM-9763] move log_graph out from runner

---
+++ autodist/utils/visualization_util.py
@@ -0,0 +1,22 @@
+"""Utility functions for graph visualization."""
+import os
+
+from tensorflow.python.summary.writer import writer
+
+import autodist.const
+from autodist.utils import logging
+
+
+def log_graph(graph, name):
+    """
+    Log the graph on Tensorboard.
+
+    Args:
+        graph: the tensorflow graph to be plotted on Tensorboard
+        name: the name of the graph
+    """
+    directory = os.path.join(autodist.const.DEFAULT_WORKING_DIR, "graphs")
+    os.makedirs(directory, exist_ok=True)
+    p = os.path.join(directory, name)
+    writer.FileWriter(p, graph=graph)
+    logging.info('Graph summary written to: %s' % p)
