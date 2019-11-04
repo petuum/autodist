432c3e7d16c2ff18f639046ae27c7fad0548bcc5
Tairui Wang <wtrcz2000@gmail.com>
2019-11-04 15:11:55 +0000

    [SYM-10050] Support all distributed test cases

--- tests/integration/test_dist.py
+++ tests/integration/test_dist.py
@@ -1,14 +1,35 @@
+import atexit
 import itertools
 import pytest
+import subprocess
 
 import os
 
-from autodist import AutoDist
-from .cases import c0
+
+cases = [
+    "c0",  # TensorFlow 2.0 basics
+    "c1",  # Keras basics
+    "c2",  # Sparse basics
+    "c3"   # Numpy basics
+]
+resource_specs = [
+    # os.path.join(os.path.dirname(__file__), 'resource_specs/r0.yml'),
+    os.path.join(os.path.dirname(__file__), 'resource_specs/r1.yml'),
+    ]
+strategies = ['PS', 'PSLoadBalancing', 'PartitionedPS', 'AllReduce']
 
 
 def test_dist():
-    resource_file = os.path.join(os.path.dirname(__file__), 'resource_specs/r1.yml')
-    strategy = 'PS'
-    a = AutoDist(resource_spec_file=resource_file, strategy_name=strategy)
-    c0.main(a)
+    combinations = itertools.product(resource_specs, strategies, cases)
+    for r, s, c in combinations:
+        if s == 'AllReduce' and c not in ["c0", "c1"]:
+            continue
+        cmd = ("python /home/autodist/autodist/tests/integration/single_run.py "
+                "--case={} --strategy={} --resource={}").format(
+                    c, s, r
+                )
+        print("=====> test starts!")
+        print("=====> cmd is {}".format(cmd))
+        status = subprocess.run(args=cmd, shell=True)
+        assert status.returncode == 0,"{}, {}, {}".format(r, s, c)
+        print("=====> test success")
