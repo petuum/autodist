0cf10a9e3af168bc954424434f6dbaa3fd4d2b05
Peng Wu <pengwu100@gmail.com>
2019-11-26 20:19:55 +0000

    [SYM-9815] Test and release on built(+compiled) wheel

--- scripts/distribution_setup.sh
+++ scripts/distribution_setup.sh
@@ -5,29 +5,17 @@ node2=$NODE2
 
 # TODO: put this into a for loop
 echo "setup $node1"
-if ssh -i /tmp/credentials/id_rsa autodist@$node1 '[ -d /home/autodist/autodist ]'
-then
-    echo "autodist exists on $node1. remove repo"
-    ssh -i /tmp/credentials/id_rsa autodist@$node1 'rm -fr /home/autodist/autodist'
-fi
-echo "download autodist"
-ssh -i /tmp/credentials/id_rsa autodist@$node1 'git clone git@gitlab.int.petuum.com:internal/scalable-ml/autodist.git'
-echo "target commit hash is $CI_COMMIT_REF_NAME"
-ssh -i /tmp/credentials/id_rsa autodist@$node1 "cd /home/autodist/autodist; pwd; git checkout $CI_COMMIT_REF_NAME"
+echo "download autodist from the build artifact"
+ssh -i /tmp/credentials/id_rsa autodist@$node1 "rm -f /home/autodist/dist/autodist*.whl"
+scp -i /tmp/credentials/id_rsa dist/autodist*.whl autodist@$node1:/home/autodist/dist
 echo "install autodist"
 ssh -i /tmp/credentials/id_rsa autodist@$node1 "/home/autodist/venv/autodist/bin/pip uninstall -y autodist"
-ssh -i /tmp/credentials/id_rsa autodist@$node1 "/home/autodist/venv/autodist/bin/pip install -e /home/autodist/autodist"
+ssh -i /tmp/credentials/id_rsa autodist@$node1 "/home/autodist/venv/autodist/bin/pip install /home/autodist/dist/autodist*.whl"
 
 echo "setup $node2"
-if ssh -i /tmp/credentials/id_rsa autodist@$node2 '[ -d /home/autodist/autodist ]'
-then
-    echo "autodist exists on $node2. remove repo"
-    ssh -i /tmp/credentials/id_rsa autodist@$node2 'rm -fr /home/autodist/autodist'
-fi
-echo "download autodist"
-ssh -i /tmp/credentials/id_rsa autodist@$node2 'git clone git@gitlab.int.petuum.com:internal/scalable-ml/autodist.git'
-echo "target commit hash is $CI_COMMIT_REF_NAME"
-ssh -i /tmp/credentials/id_rsa autodist@$node2 "cd /home/autodist/autodist; pwd; git checkout $CI_COMMIT_REF_NAME"
+echo "download autodist from the build artifact"
+ssh -i /tmp/credentials/id_rsa autodist@$node2 "rm -f /home/autodist/dist/autodist*.whl"
+scp -i /tmp/credentials/id_rsa dist/autodist*.whl autodist@$node2:/home/autodist/dist
 echo "install autodist"
 ssh -i /tmp/credentials/id_rsa autodist@$node2 "/home/autodist/venv/autodist/bin/pip uninstall -y autodist"
-ssh -i /tmp/credentials/id_rsa autodist@$node2 "/home/autodist/venv/autodist/bin/pip install -e /home/autodist/autodist"
+ssh -i /tmp/credentials/id_rsa autodist@$node2 "/home/autodist/venv/autodist/bin/pip install /home/autodist/dist/autodist*.whl"
