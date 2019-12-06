22abdc28644464a74b31baf8149f58d27f2e4512
Trevin Gandhi <sonicxml@gmail.com>
2019-12-06 16:47:29 +0000

    Rewind "[SYM-10625] fix adam optimizer"
    This reverts commit a600801892026c6f99288a4b22b86d7a0c8e24e4

--- examples/hackathon_model/simple_train_scope.py
+++ examples/hackathon_model/simple_train_scope.py
@@ -8,7 +8,7 @@ from autodist import AutoDist
 
 resource_spec_file = os.path.join(os.path.dirname(__file__), '../resource_spec.yml')
 config_file = os.path.join(os.path.dirname(__file__), '../runner_config.yml')
-autodist = AutoDist(resource_spec_file, 'PS', runner_config_file=config_file)
+autodist = AutoDist(resource_spec_file, 'AllReduce', runner_config_file=config_file)
 
 vocab_size = 10000
 embedding_size = 16
@@ -40,7 +40,7 @@ class SimpleModel():
                               name='b2',
                               trainable=True,
                               dtype=tf.float32)
-        self.optimizer = tf.optimizers.Adam(learning_rate=0.2)
+        self.optimizer = tf.optimizers.Adagrad(learning_rate=0.2, initial_accumulator_value=1.0)
 
     def forward(self, x, y):
         # embedding layer
